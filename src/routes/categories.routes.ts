ecb0c9e3cc9fc9f26140e6c36bfd87a0f723c757
Ivan Seibel <me@ivanseibel.dev>
2022-10-20 20:08:43 +0100

    Project settings

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
+
+const categoriesRoutes = Router();
+
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post('/', (request, response) => {
+  try {
+    const { name, description } = request.body;
+
+    const createCategoryService = new CreateCategoryService(
+      categoriesRepository
+    );
+
+    createCategoryService.execute({ name, description });
+
+    return response.json({ message: 'Category created!' });
+  } catch (error) {
+    return response.json({ error: error.message }).status(400);
+  }
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
+});
+
+export { categoriesRoutes };
