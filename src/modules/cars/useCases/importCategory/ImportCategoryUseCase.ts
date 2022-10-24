654f5c4eb6ff9af32b9af7dec68c92de1fca1ea4
Ivan Seibel <me@ivanseibel.dev>
2022-10-25 00:24:56 +0100

    Move from array in memory to database methods

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -40,9 +40,11 @@ class ImportCategoryUseCase {
   async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file);
 
-    categories.map((category) => {
+    categories.map(async (category) => {
       const { name, description } = category;
-      const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+      const categoryAlreadyExists = await this.categoriesRepository.findByName(
+        name
+      );
 
       if (!categoryAlreadyExists) {
         this.categoriesRepository.create({ name, description });
