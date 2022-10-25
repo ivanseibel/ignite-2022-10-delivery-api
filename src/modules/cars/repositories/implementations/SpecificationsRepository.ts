888c9ed507006eefc0f909a9f7a9d2fd808fc0e9
Ivan Seibel <me@ivanseibel.dev>
2022-10-26 00:43:07 +0100

    Implement users module - Add create new user methods

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,14 +1,10 @@
 import { getRepository, Repository } from 'typeorm';
 
+import { ICreateSpecificationDTO } from '../../dtos/ICreateSpecificationDTO';
 import { Specification } from '../../entities/Specification';
-import {
-  ISpecificationsRepository,
-  ICreateSpecificationDTO,
-} from '../ISpecificationsRepository';
+import { ISpecificationsRepository } from '../ISpecificationsRepository';
 
 class SpecificationsRepository implements ISpecificationsRepository {
-  private static INSTANCE: SpecificationsRepository;
-
   private repository: Repository<Specification>;
 
   constructor() {
