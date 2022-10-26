80b827103003418c45db796e62086d9c797856ca
Ivan Seibel <me@ivanseibel.dev>
2022-10-26 22:39:50 +0100

    Add exception middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import cors from 'cors';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
 import { databaseConnect } from './database';
+import AppError from './errors/AppError';
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 import './shared/container';
@@ -18,8 +20,22 @@ async function init() {
   }
 
   app.use(express.json());
+  app.use(cors());
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
   app.use(router);
+  app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+      if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+          error: err.message,
+        });
+      }
+
+      return response.status(500).json({
+        message: `Internal server error - ${err.message}`,
+      });
+    }
+  );
 
   app.listen(3333, () => console.log('Server is running on port 3333!'));
 }
