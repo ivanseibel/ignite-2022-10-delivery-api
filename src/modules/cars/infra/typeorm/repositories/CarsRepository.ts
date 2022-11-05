6d6c9f6f47577f35d13d7f3b8d4ea854c22c20c2
Ivan Seibel <me@ivanseibel.dev>
2022-11-05 14:55:13 +0000

    Create cars repository

---
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -0,0 +1,26 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+
+import { Car } from '../entities/Car';
+
+class CarsRepository implements ICarsRepository {
+  private repository: Repository<Car>;
+
+  constructor() {
+    this.repository = getRepository(Car);
+  }
+
+  async create(data: ICreateCarDTO): Promise<void> {
+    const car = this.repository.create(data);
+
+    await this.repository.save(car);
+  }
+
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    return this.repository.findOne({ license_plate });
+  }
+}
+
+export { CarsRepository };
