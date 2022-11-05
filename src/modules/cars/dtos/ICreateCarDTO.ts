2c9d0dbbfd12684dc1bf85c50848deda5d649ede
Ivan Seibel <me@ivanseibel.dev>
2022-11-05 20:32:43 +0000

    Add missing attributes

--- src/modules/cars/dtos/ICreateCarDTO.ts
+++ src/modules/cars/dtos/ICreateCarDTO.ts
@@ -1,14 +1,14 @@
 interface ICreateCarDTO {
-  id: string;
+  id?: string;
   name: string;
   description: string;
   daily_rate: number;
   license_plate: string;
   fine_amount: number;
   brand: string;
-  available: boolean;
+  available?: boolean;
   category_id: string;
-  created_at: Date;
+  created_at?: Date;
 }
 
 export { ICreateCarDTO };
