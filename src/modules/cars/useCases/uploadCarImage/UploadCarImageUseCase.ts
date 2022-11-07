10f83c329024f920dbeba4879dc61984188ef381
Ivan Seibel <me@ivanseibel.dev>
2022-11-07 21:08:25 +0000

    Refactor use cases

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -1,6 +1,8 @@
 import { inject, injectable } from 'tsyringe';
 
+import { CarImage } from '@modules/cars/infra/typeorm/entities/CarImage';
 import { ICarImagesRepository } from '@modules/cars/repositories/ICarImagesRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 
 interface IRequest {
   car_id: string;
@@ -11,13 +13,18 @@ interface IRequest {
 class UploadCarImageUseCase {
   constructor(
     @inject('CarImagesRepository')
-    private carImagesRepository: ICarImagesRepository
+    private carImagesRepository: ICarImagesRepository,
+
+    @inject('CarsRepository')
+    private carsRepository: ICarsRepository
   ) {}
 
-  async execute({ car_id, images_name }: IRequest): Promise<void> {
-    images_name.map(async (image) => {
-      await this.carImagesRepository.create(car_id, image);
-    });
+  async execute({ car_id, images_name }: IRequest): Promise<CarImage[]> {
+    const carImages = images_name.map((image_name) =>
+      this.carImagesRepository.create(car_id, image_name)
+    );
+
+    return Promise.all(carImages);
   }
 }
 
