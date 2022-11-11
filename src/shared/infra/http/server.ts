b3423f37cb71b14b28b304e186312a37f3573d8d
Ivan Seibel <me@ivanseibel.dev>
2022-11-11 21:00:54 +0000

    Make connection and app more reusable

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,45 +1,5 @@
-import cors from 'cors';
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-import swaggerUi from 'swagger-ui-express';
+import { app, initializeDatabase } from './app';
 
-import AppError from '@shared/errors/AppError';
-import { databaseConnect } from '@shared/infra/typeorm';
-import '@shared/container';
-
-import swaggerFile from '../../../swagger.json';
-import { router } from './routes';
-
-async function init() {
-  const app = express();
-
-  const connection = await databaseConnect();
-
-  if (!connection) {
-    console.log('Error connecting to database. Exiting...');
-    process.exit(1);
-  }
-
-  app.use(express.json());
-  app.use(cors());
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-  app.use(router);
-  app.use(
-    (err: Error, request: Request, response: Response, next: NextFunction) => {
-      if (err instanceof AppError) {
-        return response.status(err.statusCode).json({
-          error: err.message,
-        });
-      }
-
-      console.log(err);
-      return response.status(500).json({
-        message: `Internal server error - ${err.message}`,
-      });
-    }
-  );
-
-  app.listen(3333, () => console.log('Server is running on port 3333!'));
-}
-
-init();
+initializeDatabase().then(() => {
+  app.listen(3333, () => console.log('Server is running!'));
+});
