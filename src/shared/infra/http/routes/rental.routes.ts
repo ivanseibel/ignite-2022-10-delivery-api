e7a1539ad520c6ac2bf88f832b86f945c77054fe
Ivan Seibel <me@ivanseibel.dev>
2022-11-15 00:15:30 +0000

    Add methods to close rentals

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { CloseRentalController } from '@modules/rentals/useCases/closeRental/CloseRentalController';
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
 
 import { ensureAdmin } from '../middlewares/ensureAdmin';
@@ -8,11 +9,13 @@ import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const closeRentalController = new CloseRentalController();
 
 // Below routes are protected by the ensureAuthenticated middleware
 rentalRoutes.use(ensureAuthenticated);
 
 rentalRoutes.post('/', createRentalController.handle);
+rentalRoutes.post('/close/:id', closeRentalController.handle);
 
 // Below routes are protected by the ensureAdmin middleware
 rentalRoutes.use(ensureAdmin);
