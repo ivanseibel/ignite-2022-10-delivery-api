13f1fd8de65b032dbb744ce0fbdfd02464725130
Ivan Seibel <me@ivanseibel.dev>
2022-11-15 00:14:07 +0000

    Implement new method toggleCarAvailability

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -13,6 +13,16 @@ class CarsRepository implements ICarsRepository {
     this.repository = getRepository(Car);
   }
 
+  async toggleCarAvailability(id: string, new_status: boolean): Promise<void> {
+    await this.repository
+      .createQueryBuilder()
+      .update()
+      .set({ available: new_status })
+      .where('id = :id')
+      .setParameters({ id })
+      .execute();
+  }
+
   async findById(id: string): Promise<Car> {
     return this.repository.findOne(id);
   }
