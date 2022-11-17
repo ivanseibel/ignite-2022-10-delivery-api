e218e028995b6111c1b082d6a6f0bafe293d3e4b
Ivan Seibel <me@ivanseibel.dev>
2022-11-17 23:11:29 +0000

    Implement send forgotten password email

---
+++ src/modules/accounts/useCases/sendForgottenPasswordMail/SendForgottenPasswordMailUseCase.ts
@@ -0,0 +1,66 @@
+import { resolve } from 'path';
+import { inject, injectable } from 'tsyringe';
+import { v4 as uuidV4 } from 'uuid';
+
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
+import AppError from '@shared/errors/AppError';
+import { IDateProvider } from '@shared/providers/DateProvider/IDateProvider';
+import { IMailProvider } from '@shared/providers/EmailProvider/IMailProvider';
+
+@injectable()
+class SendForgottenPasswordMailUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository,
+
+    @inject('DateProvider')
+    private dateProvider: IDateProvider,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider
+  ) {}
+
+  async execute(email: string): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exist');
+    }
+
+    const token = uuidV4();
+
+    await this.usersTokensRepository.create({
+      refresh_token: token,
+      user_id: user.id,
+      expires_date: this.dateProvider.addHours(3),
+    });
+
+    const templatePath = resolve(
+      __dirname,
+      '..',
+      '..',
+      'views',
+      'emails',
+      'forgotPassword.hbs'
+    );
+
+    const variables = {
+      name: user.name,
+      resetPasswordUrl: `${process.env.FORGOT_MAIL_URL}?token=${token}`,
+      appName: process.env.APP_NAME,
+    };
+
+    await this.mailProvider.sendMail(
+      email,
+      'Password recovery',
+      variables,
+      templatePath
+    );
+  }
+}
+
+export { SendForgottenPasswordMailUseCase };
