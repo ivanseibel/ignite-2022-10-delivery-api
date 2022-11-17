b287570ab8328b26a6089194c5d2df31d374deb2
Ivan Seibel <me@ivanseibel.dev>
2022-11-17 00:39:00 +0000

    Add auth settings

---
+++ src/config/auth.ts
@@ -0,0 +1,11 @@
+export default {
+  token: {
+    secret: 'd757dfd2421525dd8ba7eba884bf644a',
+    expiresIn: '15m',
+  },
+  refreshToken: {
+    secret: 'd4a88c80f5593e9fe45af086265fea30',
+    expiresInDays: 30,
+    expiresIn: '30d',
+  },
+};
