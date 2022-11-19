8e6661ab9c760ec7d6065a31ad3778e425b83cde
Ivan Seibel <me@ivanseibel.dev>
2022-11-19 20:57:07 +0000

    Prepare to use with different providers

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from 'tsyringe';
 import { CarImage } from '@modules/cars/infra/typeorm/entities/CarImage';
 import { ICarImagesRepository } from '@modules/cars/repositories/ICarImagesRepository';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { IStorageProvider } from '@shared/providers/StorageProvider/IStorageProvider';
 
 interface IRequest {
   car_id: string;
@@ -15,14 +16,15 @@ class UploadCarImageUseCase {
     @inject('CarImagesRepository')
     private carImagesRepository: ICarImagesRepository,
 
-    @inject('CarsRepository')
-    private carsRepository: ICarsRepository
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<CarImage[]> {
-    const carImages = images_name.map((image_name) =>
-      this.carImagesRepository.create(car_id, image_name)
-    );
+    const carImages = images_name.map((image_name) => {
+      this.storageProvider.saveFile(image_name, 'cars');
+      return this.carImagesRepository.create(car_id, image_name);
+    });
 
     return Promise.all(carImages);
   }
