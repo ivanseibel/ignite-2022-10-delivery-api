102b43b2acbafbf729cd061eef971b5b00a6a8a8
Ivan Seibel <me@ivanseibel.dev>
2022-11-19 19:02:10 +0000

    Increase expiration to use while testing

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,7 +1,7 @@
 export default {
   token: {
     secret: 'd757dfd2421525dd8ba7eba884bf644a',
-    expiresIn: '15m',
+    expiresIn: '15d',
   },
   refreshToken: {
     secret: 'd4a88c80f5593e9fe45af086265fea30',
