0572ed3307f4cc42171c01fde1e45f917a91beca
Ivan Seibel <me@ivanseibel.dev>
2022-11-19 18:13:34 +0000

    Create s3 bucket and policy

---
+++ terraform/s3.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket" "images" {
+  bucket = random_pet.bucket_name.id
+}
+
+resource "aws_s3_bucket_acl" "images" {
+  bucket = aws_s3_bucket.images.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "images" {
+  bucket = aws_s3_bucket.images.id
+  policy = data.aws_iam_policy_document.images.json
+}
+
+data "aws_iam_policy_document" "images" {
+  statement {
+    sid    = "AllowPublicRead"
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.images.arn}/*"]
+  }
+}
