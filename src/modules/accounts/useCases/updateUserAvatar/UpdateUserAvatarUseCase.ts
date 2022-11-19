d04ce9cdd3f66d0bf3222ca99bd6dfb6cd4c0f59
Ivan Seibel <me@ivanseibel.dev>
2022-11-19 19:02:38 +0000

    Prepare to use with different providers

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -2,7 +2,7 @@ import { injectable, inject } from 'tsyringe';
 
 import { User } from '@modules/accounts/infra/typeorm/entities/User';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
-import { deleteFile } from '@utils/file';
+import { IStorageProvider } from '@shared/providers/StorageProvider/IStorageProvider';
 
 interface IRequest {
   user_id: string;
@@ -13,15 +13,20 @@ interface IRequest {
 class UpdateUserAvatarUseCase {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ user_id, avatar_file }: IRequest): Promise<User> {
     const user = await this.usersRepository.findById(user_id);
 
     if (user.avatar) {
-      await deleteFile(`./tmp/avatar/${user.avatar}`);
+      await this.storageProvider.deleteFile(user.avatar, 'avatar');
     }
+
+    await this.storageProvider.saveFile(avatar_file, 'avatar');
 
     user.avatar = avatar_file;
 
