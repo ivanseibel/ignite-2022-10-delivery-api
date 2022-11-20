27db024442093c4cca52a924bd121c07d533169b
Ivan Seibel <me@ivanseibel.dev>
2022-11-20 21:03:11 +0000

    Ignore eslint rule

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -35,6 +35,7 @@ app.use(express.static(`${upload.tmpFolder}`));
 
 app.use(router);
 app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
       return response.status(err.statusCode).json({
