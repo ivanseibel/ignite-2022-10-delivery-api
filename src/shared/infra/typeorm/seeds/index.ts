cca6f80ce3a3fcb2286c0542fac89f022908ddeb
Ivan Seibel <me@ivanseibel.dev>
2022-11-26 00:08:01 +0000

    Improve seed script

--- src/shared/infra/typeorm/seeds/index.ts
+++ src/shared/infra/typeorm/seeds/index.ts
@@ -4,13 +4,13 @@ import { createAdminUser } from './admin';
 
 async function seed() {
   const connection = await databaseConnect('localhost');
-
   if (!connection) {
     console.log('Error connecting to database. Exiting...');
     process.exit(1);
   }
+  await createAdminUser(connection);
 
-  await createAdminUser();
+  await connection.close();
 }
 
 seed();
